a94dcfa5830f75cbd627c3a1c7549450fc2817bf
Nishant Mehta <nishmeht7@gmail.com>
2017-07-06 16:32:47 -0500

    portfolio added

---
+++ browser/react/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Portfolio extends React.Component {
+	constructor() {
+		super()
+	}
+
+	render () {
+		return (
+			<h1>What's Gucci!!!</h1>
+		)
+	}
+}
