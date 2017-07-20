d4c73ccb9d47d81e7204bfbbc02971adcc39be35
Nishant Mehta <nishmeht7@gmail.com>
2017-07-20 14:51:18 -0500

    view heights rectified

--- notes.js
+++ notes.js
@@ -14,4 +14,13 @@ ReactDom.render() is basically the root of the tree which creates the tree with 
 the react components, essentially the API/entrance point to react 
 ReactDOM is the glue between React and the DOM
 we will mainly use it only for one thing which is mounting with ReactDom.render
+
+<div className="background col-md-offset-4">
+						<div className="transbox">
+							<p>Nishant Mehta is a FullStack developer, graduating from the prestigious FullStack Academy of Code. He loves hacking through complex problems and his current weapon of choice is Javascript with all it's ammo that includes React, React-Native, Redux, Node, Express, Socket.io, gaming libraries and so much more that's being added everyday! </p>
+						</div>
+					</div> 
+
+
+https://www.facebook.com/dialog/oauth?app_id=175885359193859&auth_type=&client_id=175885359193859&display=popup&domain=test.local&e2e=%7B%7D&locale=en_US&origin=1&redirect_uri=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FXBwzv5Yrm_1.js%3Fversion%3D42%23cb%3Df9502468491c08%26domain%3Dtest.local%26origin%3Dhttp%253A%252F%252Ftest.local%253A3000%252Ff13030cb815147c%26relation%3Dopener%26frame%3Df1ba7e283f649d4&ref=LoginButton&response_type=none%2Ctoken%2Csigned_request&scope=read_stream%2Cpublish_stream&sdk=joey&seen_revocable_perms_nux=false
 */
