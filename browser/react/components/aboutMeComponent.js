3968bff1b298ad8fe9c4be55340be099e10a427f
Nishant Mehta <nishmeht7@gmail.com>
2017-07-26 16:44:59 -0500

    aboutMe changed

--- browser/react/components/aboutMeComponent.js
+++ browser/react/components/aboutMeComponent.js
@@ -26,15 +26,16 @@ export default class AboutMeComponent extends React.Component {
 						<div className="about-section">
 						<div className="col-md-6">
 						<div className="aboutText">
-							<p>
-								I love problem solving. There's nothing more exhilarating than hacking through complex problems Except of course jumping from a plane at 20,000 feet above the palm islands in Dubai!
-								<br /></p>
-								<p>Becoming the best developer is my goal and my current weapon of choice is Javascript with all it's ammo that includes React, React-Native, Redux, Node, Express, Socket.io, gaming libraries and so much more that's being added everyday!
-								<br /> 
-								I have extensive experience in the tech industry, having grown a company of my own I'm addicted to the sweat, hustle and dedication that goes into creating a winning team and succeeding!
-								<br /> 
-								HIRE ME!!!!!
-							</p>
+							<ul>
+								<li>I Really Like Problem Solving </li>
+								<li>I Like High Adrenaline Activities</li>
+								<li>I Love Javascript</li>
+								<li>I'm a founding member of a tech company</li>
+								<li>I grew it to 13 people and $1M+ in funding</li>
+								<li>I am addicted to the sweat and hustle to WIN</li>
+								<li>I enjoy working with a goal oriented team</li>
+								<li>Above all I love pizza</li>
+							</ul>
 							</div>
 						</div>
 						<div className="col-md-offset-6">
@@ -49,3 +50,12 @@ export default class AboutMeComponent extends React.Component {
 		)
 	}
 }
+/*<p>
+								I love problem solving. There's nothing more exhilarating than hacking through complex problems Except of course jumping from a plane at 20,000 feet above the palm islands in Dubai!
+								<br /></p>
+								<p>Becoming the best developer is my goal and my current weapon of choice is Javascript with all it's ammo that includes React, React-Native, Redux, Node, Express, Socket.io, gaming libraries and so much more that's being added everyday!
+								<br /> 
+								I have extensive experience in the tech industry, having grown a company of my own I'm addicted to the sweat, hustle and dedication that goes into creating a winning team and succeeding!
+								<br /> 
+								HIRE ME!!!!!
+							</p>*/
